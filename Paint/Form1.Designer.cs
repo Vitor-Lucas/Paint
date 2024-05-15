71796d5042269a1bbbfdbf74c4e55dd9533c37aa
V! <125708505+Vitor-Lucas@users.noreply.github.com>
2024-05-15 10:40:09 -0300

    mudancas

--- Paint/Form1.Designer.cs
+++ Paint/Form1.Designer.cs
@@ -28,13 +28,102 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(24, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(52, 40);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "rectangule";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(82, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(52, 40);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "triangule";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(140, 13);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(52, 39);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "circle";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(198, 13);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(53, 40);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "elipse";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(257, 12);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(53, 40);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "losangle";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(316, 13);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(53, 40);
+            this.button6.TabIndex = 5;
+            this.button6.Text = "pentagono";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseClick);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
     }
 }
 
--- Paint/Form1.cs
+++ Paint/Form1.cs
@@ -12,9 +12,165 @@ namespace Paint
 {
     public partial class Form1 : Form
     {
+
+        int qtd_clicks = 0;
+        int x0, y0, x1, y1, x2, y2;
+
+        String operation;
+
         public Form1()
         {
             InitializeComponent();
+        }
+        Pen GetPen(int r, int g, int b)
+        {
+            Pen pen = new Pen(Color.FromArgb(r, g, b));
+            //pen.DashPattern = pattern;
+
+            return pen;
+        }
+        void DrawLine(PaintEventArgs e, Pen pen, int x0, int y0, int x1, int y1)
+        {
+            e.Graphics.DrawLine(pen, x0, y0, x1, y1);
+        }
+        
+        void DrawPoint(PaintEventArgs e, Pen pen, int x, int y)
+        {
+            e.Graphics.DrawLine(pen, x, y, x+1, y);
+        }
+
+        void DrawArc(PaintEventArgs e, Pen pen, int x_center, int y_center, int radius, int initial_angle, int final_angle)
+        {
+            for(float thetha = initial_angle; thetha <= final_angle; thetha+= 0.33f)
+            {
+                double theta_radians = thetha * Math.PI / 180;
+                int x = (int) (x_center + radius * Math.Cos(theta_radians));
+                int y = (int) (y_center + radius * Math.Sin(theta_radians));
+
+                DrawPoint(e, pen, x, y);
+            }
+        }
+
+        void DrawArc(PaintEventArgs e, Pen pen, int x_center, int y_center, int radius_x, int radius_y, int initial_angle, int final_angle)
+        {
+            for (float thetha = initial_angle; thetha <= final_angle; thetha += 0.33f)
+            {
+                double theta_radians = thetha * Math.PI / 180;
+                int x = (int)(x_center + radius_y * Math.Cos(theta_radians));
+                int y = (int)(y_center + radius_x * Math.Sin(theta_radians));
+
+                DrawPoint(e, pen, x, y);
+            }
+        }
+
+        void DrawRectangle(PaintEventArgs e, Pen pen, int x, int y, int width, int height)
+        {
+            e.Graphics.DrawRectangle(pen, x, y, width, height);
+        }
+
+        void DrawTriangle(PaintEventArgs e, Pen pen, int x0, int y0, int x1, int y1, int x2, int y2)
+        {
+            DrawLine(e, pen, x0, y0, x1, y1);
+            DrawLine(e, pen, x1, y1, x2, y2);
+            DrawLine(e, pen, x2, y2, x0, y0);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            //operation = "Draw circle";
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            //operation = "Draw elipse";
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            //operation = "Draw losangle";
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            //operation = "Draw pentagono";
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            Pen pen = GetPen(0, 0, 0);
+
+           //DrawArc(e, pen, 100, 100, 100, 200, 0, 360);
+
+
+            switch (operation)
+            {
+                case "Draw Triangle":
+                    DrawTriangle(e, pen, x0, y0, x1, y1, x2, y2); break;
+                case "Draw Rectangle":
+                    DrawRectangle(e, pen, x0, y0, (x1 - x0), (y1 - y0)); break;
+               /* case "Draw Circle":
+                    DrawArc(e, pen, x0, y0, r,0,360); break;
+                case "Draw elipse":
+                    DrawArc(e, pen, x0, y0, r_x, r_y, 0, 360); break;
+               case "Draw losangle":
+                    DrawLosangle(); break;
+               case "Draw pentagono":
+                    DrawPentagono(); break;
+                
+                */
+
+
+            }
+            
+        }
+
+
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            operation = "Draw Triangle";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            operation = "Draw Rectangle";
+        }
+
+
+        private void Form1_MouseClick(object sender, MouseEventArgs e)
+        {
+            qtd_clicks++;
+            if (qtd_clicks == 3)
+            {
+                qtd_clicks = 0;
+                x2 = e.X; y2 = e.Y;
+                if (operation == "Draw Triangle")
+                    Invalidate();
+                
+                    
+            }
+            else if (qtd_clicks == 2)
+            {
+                x1 = e.X; y1 = e.Y;
+                if (operation == "Draw Rectangle")
+                {
+                    qtd_clicks = 0;
+                    Invalidate();
+                }
+                    
+                
+            }
+            else if (qtd_clicks == 1)
+            {
+                x0 = e.X; y0 = e.Y;
+                
+            }
+
         }
     }
 }
